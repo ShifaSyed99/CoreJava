b61ed9fe795cdb7425a9298f96ddc5e401fdc964
LENOVO <LENOVO@LENOVO-PC-3>
2024-02-22 11:04:28 +0530

    h

--- JUnit/src/com/edubridge/junitdemo/AnnotationsDemo.java
+++ JUnit/src/com/edubridge/junitdemo/AnnotationsDemo.java
@@ -1,7 +1,5 @@
 package com.edubridge.junitdemo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
