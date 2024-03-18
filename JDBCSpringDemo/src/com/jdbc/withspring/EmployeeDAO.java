1bf647db94aa3d4127e8f43cf9dac38b95d7a5f6
ShifaSyed99 <145594541+ShifaSyed99@users.noreply.github.com>
2024-03-18 23:57:38 +0530

    EmployeeDao.java

--- JDBCSpringDemo/src/com/jdbc/withspring/EmployeeDAO.java
+++ JDBCSpringDemo/src/com/jdbc/withspring/EmployeeDAO.java
@@ -1,17 +1,26 @@
 package com.jdbc.withspring;
 
-public class EmployeeDAO {
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class EmployeeDao {
 	
-	private org.springframework.jdbc.core.JdbcTemplate JdbcTemplate;
-	
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate)
-	{
-		this.JdbcTemplate = jdbcTemplate;
+	private JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
-	public int saveEmployee(Employee e)
-	{
-		String query = "insert into employee values"('"+e.getId()+"','"+e.getName()+"','"+e.getSalary()+"')";
+	public int saveEmployee(Employee e){
+		String query="insert into employee values('"+e.getId()+"','"+e.getName()+"','"+e.getSalary()+"')";
 		return jdbcTemplate.update(query);
 	}
-}
+	public List<Employee>employee()
+	{
+		String sql="select * from employee";
+		List<Employee>employees=jdbcTemplate.query(sql,new EmployeeMapper());
+		return employees;
+	}
+	
+}	
