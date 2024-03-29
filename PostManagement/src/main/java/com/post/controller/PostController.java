885acfaa7d1ebaf8401056ba07e57f48a178d95b
ShifaSyed99 <145594541+ShifaSyed99@users.noreply.github.com>
2024-03-29 13:26:41 +0530

    new

--- PostManagement/src/main/java/com/post/controller/PostController.java
+++ PostManagement/src/main/java/com/post/controller/PostController.java
@@ -1,7 +1,14 @@
 package com.post.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +24,7 @@ public class PostController {
 	@Autowired
 		PostRepository repo;
 	
-@PostMapping
+@PostMapping("/addpost")
 
 	public Post createpost(@RequestBody Post p) {
 		
@@ -25,4 +32,32 @@ public class PostController {
 	
 }
 
+@GetMapping("/readpost")
+public List<Post> fetchAllPostDetails() {
+	 List<Post> allpost = repo.findAll();
+	return allpost;
+
 }
+
+@GetMapping("/readpost/{id}")
+public Optional<Post>  getPostById(@PathVariable("id") int id) {
+	return repo.findById(id);
+}
+
+@DeleteMapping("/deletepost/{id}")
+public String deletePost(@PathVariable("id") int id) {
+	if(repo.findById(id).isPresent()) {
+		repo.deleteById(id);
+		return "Post Deleted Successfully";
+	}
+	return "Post is Not Available";
+}
+
+@PutMapping("/updatepost/{id}")
+public Post  updatepost(@PathVariable("id") int id, @RequestBody Post p) {
+	p.setPostId(id);
+	return repo.save(p);
+}
+
+}
+
